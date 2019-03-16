a22ae2bb3081700523d88641c091cdf966042912
Wang <wangjingchen155@163.com>
2019-03-17 00:53:02 +0800

    前车等待修改

--- main.cpp
+++ main.cpp
@@ -9,40 +9,63 @@
 #include <cstdio>
 #include "input.h"
 #include <vector>
-#define normal 1
+#include <map>
+#define normal -1
 #define wait 2
 #define finish 3
 #define terminal 4
 using namespace std;
-vector<int> isroadempty(const vector<Road> &roads,int num_road,int from);
+vector<int> isroadempty(map<int, Road* > m_road,map<int, Car*> m_car,int num_road,int id_car);
 
-vector<int> isroadempty(const vector<Road> &roads,int num_road, int from) {
+vector<int> isroadempty(map<int,Road* > m_road,map<int, Car*> m_car,int num_road,int id_car) {
     int num_roads=0;
     vector<int>res;
-    int num_channel=roads.size();
-    for(int i=0;i<roads.size();++i)
+    Road *roads=(m_road.find(num_road)->second);
+    int num_channel=0;
+    int car_front=-1;
+    int from=m_car.find(id_car)->second->from;
+    if((*roads).Carline2.size()==0||(*roads).Carline.size()==0)
     {
-        if(roads[i].idx==num_road)
-        {
-            num_roads=i;
-            break;
-        }
+        car_front=-1;
+        res.push_back(num_channel);
+        res.push_back(car_front);
+        res.push_back(1);
+        return  res;
     }
-    if(roads[num_roads].from==from)
+    if((*roads).from==from)
     {
-
-
-        int car_front=0;
-        if(roads[num_roads].Carline[num_channel].size()<roads[num_roads].length)
+        num_channel=(*roads).Carline.size()-1;
+        if((*roads).Carline.size()<(*roads).length)
         {
-            car_front=roads[num_roads].Carline[num_channel][roads[num_roads].Carline[num_channel].size()-1];
+            car_front=(*roads).Carline[(*roads).Carline.size()-1][(*roads).Carline[(*roads).Carline.size()-1].size()-1];
         } else{
             car_front=-1;
+            ++num_channel;
         }
 
         res.push_back(num_channel);
         res.push_back(car_front);
+        if((*roads).Carline[(*roads).Carline.size()-1].size()<(*roads).length)
+            res.push_back(1);
+        else
+            res.push_back(0);
 
+    } else{
+        num_channel=(*roads).Carline2.size()-1;
+        if((*roads).Carline2.size()<(*roads).length)
+        {
+            car_front=(*roads).Carline2[(*roads).Carline2.size()-1][(*roads).Carline2[(*roads).Carline2.size()-1].size()-1];
+        } else{
+            car_front=-1;
+            ++num_channel;
+        }
+
+        res.push_back(num_channel);
+        res.push_back(car_front);
+        if((*roads).Carline2[(*roads).Carline2.size()-1].size()<(*roads).length)
+            res.push_back(1);
+        else
+            res.push_back(0);
     }
     return res;
 }
@@ -72,31 +95,50 @@ int main() {
     answers=Car_answer_input(path4);
 //    for (auto iter=answers.cbegin();iter !=answers.cend();iter++)
 //        cout<<(*iter).idx<<(*iter).planTime<<(*iter).road_id.front()<<endl;
+    map<int, Car*> car_map;//car_map从汽车标号到cars下标的映射
+    for(int i=0;i<cars.size();++i)
+        car_map.insert(pair<int ,Car*>(cars[i].idx,&cars[i]));
+
+    map<int, Road* > road_map;//road_map从road标号到roads下标的映射
+    for(int i=0;i<roads.size();++i)
+        road_map.insert(pair<int ,Road*>(roads[i].idx,&roads[i]));
+
+    map<int, Cross* > corss_map;//corss_map从corss标号到corsses下标的映射
+    for(int i=0;i<crosses.size();++i)
+        corss_map.insert(pair<int ,Cross*>(crosses[i].idx,&crosses[i]));
+
     int time=1;
 
-//    for(int i=0;i<answers.size();++i)
-//    {
-//        if(answers[i].planTime>time)
-//        {
-//            cars[i].state==finish;
-//            continue;
-//        }
-//        int road_num=isroadempty();//返回车道号，前车ID（注意一车道满，二车道首车的情况）
-//
-//        if(road_num) //返回可进入的车道号；
-//        {
-//            //车入车道
-//            cars[i].state==finish;
-//            更改车的R和V
-//
-//
-//        } else{
-//            cars[i].state==finish;
-//
-//        }
-//
-//    }
+    for(int i=0;i<answers.size();++i)
+    {
+        if(answers[i].planTime>time)
+        {
+            cars[i].state==normal;
+            continue;
+        }
+        vector<int> road_num=isroadempty(road_map,car_map,answers[i].road_id[0],answers[i].idx);//返回车道号，前车ID（注意一车道满，二车道首车的情况）
+                                                                                                //车道号，前车id ，有没有空位（0/1）
+        if(road_num[2]) //返回可进入的车道号；
+        {
+            //车入车道
+            cars[i].state=finish;
 
+            cars[i].road=answers[i].road_id[0];
+            cars[i].r=road_map[(car_map.find(answers[i].idx)->second->road)]->speed;
+            cars[i].speed=road_map[car_map[answers[i].idx]->road]->speed;
+            cars[i].pre=road_num[1];
+            //更改车的R和V
+
+
+        } else{
+            cars[i].state=finish;
+
+        }
+
+    }
+    for (auto iter=cars.cbegin();iter !=cars.cend();iter++)
+        cout<<(*iter).idx<<", "<<(*iter).road<<","<<(*iter).r<<","<<(*iter).pre<<endl;
+    system("pause");
 
 
 
